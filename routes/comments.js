7aace7e41b16b92a38325a34a06bb9868032e8d9
Emmanuel Olivo <emmanuelo1002@gmail.com>
2018-05-16 17:06:30 -0500

    Comment destroy

--- routes/comments.js
+++ routes/comments.js
@@ -75,6 +75,17 @@ router.put("/:comment_id", function(req, res) {
   });
 });
 
+// DESTROY - delete comment route
+router.delete("/:comment_id", function(req, res) {
+  Comment.findByIdAndRemove(req.params.comment_id, function(err) {
+    if (err) {
+      res.redirect("back");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  });
+});
+
 
 // MIDDLEWARE
 function isLoggedIn(req, res, next) {
