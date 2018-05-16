9ad05f03ba73ee457dd0849796e17ec59e4eeb8d
Emmanuel Olivo <emmanuelo1002@gmail.com>
2018-05-16 13:54:54 -0500

    Refactor routes

---
+++ routes/comments.js
@@ -0,0 +1,56 @@
+const express = require('express');
+var router = express.Router({mergeParams: true});
+
+const Comment = require('../models/comment');
+const Campground = require('../models/campground');
+
+// COMMENTS ROUTES
+
+//NEW - send to form to create comments
+router.get("/new", isLoggedIn, function(req, res) {
+  //Find campground
+  Campground.findById(req.params.id, function(err, returnedCamp) {
+    if (err) {
+      console.log(err);
+    } else {
+
+      res.render("comments/new", {campground: returnedCamp});
+    }
+  });
+});
+
+// CREATE - create the comment from the form
+router.post("/", isLoggedIn, function(req, res) {
+  //Find campground
+  Campground.findById(req.params.id, function(err, returnedCamp) {
+    if (err) {
+      console.log(err);
+    } else {
+      // Create comment
+      Comment.create(req.body.comment, function(err, createdComment) {
+        if (err) {
+          console.log(err);
+        } else {
+          // Associate comment to campground
+          returnedCamp.comments.push(createdComment);
+          returnedCamp.save();
+
+          // Redirect to campground
+          res.redirect("/campgrounds/" + returnedCamp._id);
+        }
+      });
+
+    }
+  });
+});
+
+// MIDDLEWARE
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
+
+
+module.exports = router;
